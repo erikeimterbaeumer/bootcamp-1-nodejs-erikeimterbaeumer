6dedacd1ad8a5b4c33d157a5d0b1ace0241a8bb2
Erik EImterbäumer <erik@animarum.de>
2019-01-22 19:43:03 -0500

    server response for listings

--- server.js
+++ server.js
@@ -6,8 +6,29 @@ var http = require('http'),
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
+var requestHandler = (request, response) => {
+  var parsedUrl = url.parse(request.url).pathname;
+  const statusCode = response.statusCode;
+
+  switch (parsedUrl) {
+    case "/listings":
+      response.writeHead(200, {'Content-Type': 'text/json'});
+      response.write(listingData); 
+      break;
+  
+    default:
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.write('Bad gateway error'); 
+      break;
+  }
+
+  // if (parsedUrl == "/listings") {
+  //   response.writeHead(200, {'Content-Type': 'text/json'});
+  //   response.write(listingData);    
+  // } else {
+  //   response.writeHead(404, {'Content-Type': 'text/plain'});
+  //   response.write('Bad gateway error'); 
+  // }
 
   /*
     Your request handler should send listingData in the JSON format if a GET request 
@@ -16,11 +37,23 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+  response.end();
 };
+
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+  listingData = data;
+
+  // a server is created, but not started
+  server = http.createServer(requestHandler);
+
+  // the server is now started, listening for requests on port 8080
+  server.listen(port, function() {
+    //once the server is listening, this callback function is executed
+    console.log('Server listening on: http://127.0.0.1:' + port);
+  });
 });
